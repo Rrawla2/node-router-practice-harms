7ba3aceff5211eca7088928ff61df2c663d3671b
Kristi Gribble <kgribble4083@gmail.com>
2020-03-15 19:40:50 -0500

    endpoints written

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const quotesRouter = require("../quotes/quotes-router");
 
 const server = express();
+
 server.use(cors());
+server.use(express.json());
+
+server.use('/quotes', quotesRouter);
+server.get('/', (req, res) => {
+    res.send('We are receiving data from request api');
+})
 
 
 module.exports = server;
--- quotes/quotes-router.js
+++ quotes/quotes-router.js
@@ -1,3 +1,69 @@
 const Quotes = require('../quotes/quotes-model');
 const express = require("express");
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    Quotes.getAll()
+        .then(quotes => {
+            res.status(200).json(quotes)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Server Error' })
+        })
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    Quotes.insert(req.body)
+        .then(quote => {
+            res.status(201).json(quote)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Server Error on POST' })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Quotes.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json({ message: `Quote ${id} was deleted`, deleted })
+            } else {
+                res.status(404).json({ message: `The post with the specified id does not exist.` })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Delete Server Error" })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    
+    Quotes.update(changes, id)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json({ message: "Quote was successfully updated", changes })
+            } else {
+                res.status(404).json({ message: "Quote with the specified id does not exist" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Quote Info could not be modified" })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Quotes.findById(id)
+        .then(quote => {
+            res.status(200).json(quote)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Quote does not exist"})
+        })
+})
+
+module.exports = router
